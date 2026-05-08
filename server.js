2fe49eca62d26a9cf812e2bcc1ddfcd7e284edc2
HumayE <49028632+HumayE@users.noreply.github.com>
2026-05-08 18:55:15 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.post("/login", async (req, res) => {
+  const { name } = req.body;
+
+  const result = await pool.query(
+    "INSERT INTO users(display_name) VALUES($1) RETURNING *",
+    [name]
+  );
+
+  res.json(result.rows[0]);
+});
+
+app.post("/tasks", async (req, res) => {
+  const { title, user_id } = req.body;
+
+  const result = await pool.query(
+    "INSERT INTO tasks(title, user_id) VALUES($1, $2) RETURNING *",
+    [title, user_id]
+  );
+
+  res.json(result.rows[0]);
+});
+
+app.get("/tasks/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+
+  const result = await pool.query(
+    "SELECT * FROM tasks WHERE user_id = $1",
+    [user_id]
+  );
+
+  res.json(result.rows);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server running 🚀");
+});
