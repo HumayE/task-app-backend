2eb48af007a5b39289c729fb3183afe287082d42
HumayE <49028632+HumayE@users.noreply.github.com>
2026-05-10 19:15:55 +0300

    initial backend

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.listen(PORT)
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
