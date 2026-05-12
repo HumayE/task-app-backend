a12215e5ddfeb96b0ec3bdbafbd2eed95b72c9e9
HumayE <49028632+HumayE@users.noreply.github.com>
2026-05-12 11:28:22 +0300

    initial backend

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.listen(PORT)
+const PORT = process.env.PORT || 3000;
+
+
 
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
